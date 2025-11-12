536f7b1fe3d2c0449fceba857a110a1293e7ce10
Ajay Kumar Rana <ajayrana3501@gmail.com>
2025-11-12 12:07:29 +0530

    Add files via upload

---
+++ pig_dice_roll_p6.py
@@ -0,0 +1,59 @@
+                 # ================================================================
+                                      # Roll the Dice
+                 # ================================================================
+
+import random
+
+def roll():                                 # Created a function and assigned rolling conditions
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+while True:
+    players = input("Enter the number of players (2 - 4) ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between (2 - 4) players. ")
+    else :
+        print("Invalid ! try again.")
+
+
+max_score = 50
+
+player_score = [0 for _ in range(players)]
+
+while max(player_score) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer number ",player_idx + 1,"turn has just started.\n")
+        print("your total score is", player_score[player_idx],"\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll if yes then type (y) ? ").lower()
+            if should_roll != "y":
+                break
+            
+            value = roll()
+            if value == 1:
+                print("You rolled 1 ! ,turn Done .")
+                current_score = 0
+                break
+
+            else:
+                current_score += value
+                print("You rolled a",value)
+
+            print("Your score is :",current_score)
+
+        player_score[player_idx] += current_score
+        print("Your total score is :", player_score[player_idx])
+        
+
+max_score = max(player_score)
+winning_idx = player_score.index(max_score)
+print("Player number",winning_idx + 1 ,"is the winner with the score of",max_score)
